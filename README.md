9b2c5c3164d04cbcab27e428f427fa31c3f31244
dmytropakki1995 <dimapakki95@gmail.com>
2026-03-12 00:02:40 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# webapp-demo
-This repo is used as a demo for School  You should be able to use Python venv and pip for package installation
+### webapp
+This repo is used as a demo for School 
+You should be able to use Python venv and pip for package installation
+
+
+### 🧰 Requirements
+- **python** version 3.12 or later
+
+### 🛠 Run Python application
+```sh
+# Create venv
+python -m venv venv
+
+## Activate vevn
+# Linux
+source venv/bin/activate
+# Windows
+venv\Scripts\activate
+
+# install dependencies
+uv sync
+
+# Activate .venv
+source .venv/bin/activate
+
+# Install packages
+pip install -r requirements.txt
+
+# Run application
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+HTML = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Calculator</title>
+</head>
+<body>
+
+<h2>Простий калькулятор</h2>
+
+<form method="post">
+    <input type="number" step="any" name="a" required>
+    
+    <select name="op">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+
+    <input type="number" step="any" name="b" required>
+
+    <button type="submit">Обчислити</button>
+</form>
+
+{% if result is not none %}
+<h3>Результат: {{result}}</h3>
+{% endif %}
+
+</body>
+</html>
+"""
+
+@app.route("/", methods=["GET", "POST"])
+def calculator():
+    result = None
+
+    if request.method == "POST":
+        a = float(request.form["a"])
+        b = float(request.form["b"])
+        op = request.form["op"]
+
+        if op == "+":
+            result = a + b
+        elif op == "-":
+            result = a - b
+        elif op == "*":
+            result = a * b
+        elif op == "/":
+            if b == 0:
+                result = "Ділення на нуль!"
+            else:
+                result = a / b
+
+    return render_template_string(HTML, result=result)
+
+
+if name == "__main__":
+    app.run(debug=True)
