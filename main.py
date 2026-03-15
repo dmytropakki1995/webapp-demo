040cf0a13fa17647339c16b4930fe075353cfb0f
dmytropakki1995 <dimapakki95@gmail.com>
2026-03-15 10:46:47 +0200

    upd code

--- main.py
+++ main.py
@@ -1,65 +1,18 @@
-from flask import Flask, request, render_template_string
+a = int(input("Entre first number: "))
+b = int(input("Entre second number: "))
 
-app = Flask(__name__)
+operation = input("Entre operation: ")
 
-HTML = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Simple Calculator</title>
-</head>
-<body>
+if operation == "+":
+    result = a + b
+elif operation == "-":
+    result = a - b
+elif operation == "*":
+    result = a * b
+elif operation == "/":
+    if b == 0:
+        result = "Ділення на нуль!"
+    else:
+        result = a / b
 
-<h2>Простий калькулятор</h2>
-
-<form method="post">
-    <input type="number" step="any" name="a" required>
-    
-    <select name="op">
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-    </select>
-
-    <input type="number" step="any" name="b" required>
-
-    <button type="submit">Обчислити</button>
-</form>
-
-{% if result is not none %}
-<h3>Результат: {{result}}</h3>
-{% endif %}
-
-</body>
-</html>
-"""
-
-@app.route("/", methods=["GET", "POST"])
-def calculator():
-    result = None
-
-    if request.method == "POST":
-        a = float(request.form["a"])
-        b = float(request.form["b"])
-        op = request.form["op"]
-
-        print("Test logs")
-
-        if op == "+":
-            result = a + b
-        elif op == "-":
-            result = a - b
-        elif op == "*":
-            result = a * b
-        elif op == "/":
-            if b == 0:
-                result = "Ділення на нуль!"
-            else:
-                result = a / b
-
-    return render_template_string(HTML, result=result)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+print(result)
